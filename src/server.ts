ff4892a1595fafd41a344ea21a3f794ddfe7bcd5
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-06-07 22:07:40 +0700

    chore: Add @types/cors to devDependencies and dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import { router } from "express-file-routing"
 import path from "path"
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import cors from "cors";
 const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+app.use(cors())
 app.use("/", await router({
     directory: path.join(__dirname, "routes"),
 }))
